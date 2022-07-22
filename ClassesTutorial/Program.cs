15498eb5ef866aea4494ddf9329c3d99b9c6d689
sarahemaginnis <sarahemaginnis@gmail.com>
2022-07-22 10:13:30 -0500

    Add project files.

---
+++ ClassesTutorial/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Classes
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person person = new Person();
+            var person = Person.Parse("John");
+            //person.Name = "John";
+            person.Introduce("Mosh");
+        }
+    }
+}
